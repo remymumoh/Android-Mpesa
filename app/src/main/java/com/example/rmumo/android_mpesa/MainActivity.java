7d1f309ce3a2717c5847ad9935a88fdf0b878126
Raymond Mumo <RMumo@sidianbank.co.ke>
2018-06-27 10:12:36 +0300

    First Commit

---
+++ app/src/main/java/com/example/rmumo/android_mpesa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rmumo.android_mpesa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
